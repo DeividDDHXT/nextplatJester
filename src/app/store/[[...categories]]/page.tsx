35ea78d2404f6845967fa1b082470942eb878577
DeividDDHXT <jestmon02@gmail.com>
2024-02-24 18:52:05 -0600

    05 ParametrosRutas

---
+++ src/app/store/[[...categories]]/page.tsx
@@ -0,0 +1,21 @@
+interface CategoryProps{
+    params: {
+
+        categories:string[],
+        searchParams: string
+    }
+}
+
+
+export default function Category(props:CategoryProps){
+
+    const {categories}= props.params
+    console.log(props)
+    console.log(categories)
+
+    return(
+
+
+        <h1>Category dinamica {categories}</h1>
+    )
+}
