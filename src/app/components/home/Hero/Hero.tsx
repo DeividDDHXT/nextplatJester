4aabf4466c1e02517d8a5ad8b0086680602f8ddf
DeividDDHXT <jestmon02@gmail.com>
2024-03-04 08:06:24 -0600

    2-1 ModulesEnNext

--- src/app/components/home/Hero/Hero.tsx
+++ src/app/components/home/Hero/Hero.tsx
@@ -1,9 +1,12 @@
+import styles from './Hero.module.css'
+
 export const Hero = () =>{
 
     return(
-<section>
+<section className={styles.Hero}>
 
-<h1>Hero</h1>
+<h1>Future world</h1>
+<h2> Empowering your future, today!</h2>
 
 </section>
 
