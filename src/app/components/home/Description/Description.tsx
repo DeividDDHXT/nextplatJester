fe91ac652d2054688bb27d2ffc655bfad8cbe0fa
DeividDDHXT <jestmon02@gmail.com>
2024-03-04 09:23:01 -0600

    2-4 estaticosSass

--- src/app/components/home/Description/Description.tsx
+++ src/app/components/home/Description/Description.tsx
@@ -5,7 +5,9 @@ export const Descripcion = () =>{
 
         <section>
 
-        <h1>Descripcion</h1>
+        <img src="./images/description.jpg" alt="product markerplace" />
+        <h2>Descriptions</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, voluptates sapiente, doloribus minus impedit nihil dolore deserunt quam distinctio est consectetur sed vero temporibus exercitationem repellendus, ad animi! Natus, minus.</p>
         
         </section> 
 
