0329e7d3da1e9db3dc13fc260dcf174cfb7967e2
DeividDDHXT <jestmon02@gmail.com>
2024-03-04 09:45:30 -0600

    2-5 imagenOptimaSass

--- src/app/components/home/Description/Description.tsx
+++ src/app/components/home/Description/Description.tsx
@@ -1,14 +1,31 @@
+import styles from './Description.module.sass'
+import Image from 'next/image'
+
 export const Descripcion = () =>{
 
 
     return(
 
-        <section>
+        <section className={styles.Description}>
 
-        <img src="./images/description.jpg" alt="product markerplace" />
-        <h2>Descriptions</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, voluptates sapiente, doloribus minus impedit nihil dolore deserunt quam distinctio est consectetur sed vero temporibus exercitationem repellendus, ad animi! Natus, minus.</p>
+        <Image 
+        src="/images/description.jpg" 
+        alt="product markerplace" 
+        width={500} 
+        height={300} 
+       
         
+        />
+
+<div className={styles.Descripcion__text}>
+
+<h2>Descriptions</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, voluptates sapiente, doloribus minus impedit nihil dolore deserunt quam distinctio est consectetur sed vero temporibus exercitationem repellendus, ad animi! Natus, minus.</p>
+
+
+</div>
+    
         </section> 
 
     )
