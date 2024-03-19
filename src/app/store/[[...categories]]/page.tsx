eefb6d1060030f4a1cdc50f23784b4920eb08c84
DeividDDHXT <jestmon02@gmail.com>
2024-03-18 19:58:44 -0600

    4-6 vieweProductos

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,21 +1,42 @@
-interface CategoryProps{
-    params: {
+// interface CategoryProps{
+//     params: {
 
-        categories:string[],
-        searchParams: string
-    }
+//         categories:string[],
+//         searchParams: string
+//     }
+// }
+
+
+// export default function Category(props:CategoryProps){
+
+//     const {categories}= props.params
+//     console.log(props)
+//     console.log(categories)
+
+//     return(
+
+
+//         <h1>Category dinamica {categories}</h1>
+//     )
+// }
+import { ProductsWrapper } from "../../../components/Store/ProductWrapper"
+// import { getCollectionProducts, getCollections } from "app/services/shopify/collections"
+import { getProducts } from "../../../services/shopify"
+
+interface CategoryProps {
+  params: {
+    categories: string[],
+  }
+  searchParams?: string
 }
 
-
-export default function Category(props:CategoryProps){
-
-    const {categories}= props.params
-    console.log(props)
-    console.log(categories)
-
-    return(
+export default async function Category(props: CategoryProps) {
+  const products = await getProducts()
+  const { categories } = props.params
 
 
-        <h1>Category dinamica {categories}</h1>
-    )
+
+  return (
+    <ProductsWrapper products={products} />
+  )
 }
