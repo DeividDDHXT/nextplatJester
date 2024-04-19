b5e91abd282624a65bc23b357fc8cedc50c929de
DeividDDHXT <jestmon02@gmail.com>
2024-04-19 13:05:18 -0600

    4-15 seo

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -3,10 +3,24 @@ import { getProducts } from "app/services/shopify/products"
 import { redirect } from "next/navigation"
 
 
-
 interface ProductPageProps {
   searchParams: {
     id: string
+  }
+}
+
+export async function generateMetadata({ searchParams }: ProductPageProps) { 
+  const id = searchParams.id
+  const products = await getProducts(id)
+  const product = products[0]
+
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      images: [product.image]
+    }
   }
 }
 
@@ -15,11 +29,10 @@ export default async function ProductPage({ searchParams }: ProductPageProps) {
   const products = await getProducts(id)
   const product = products[0]
 
-  // if(!id){
 
-  //   redirect('/')
-
-  // }
+  if (!id) {
+    redirect('/')
+  }
 
   return <ProductView product={product} />
 }
